63fd65b64e34251f29538252202938e13d63cbfa
rolecram <48367010+rolecram@users.noreply.github.com>
2021-05-21 22:41:41 -0300

    delete webserver

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 //const https = require('https');
 const http = require('http');
 //const fs = require('fs');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const app = express()
 
 
